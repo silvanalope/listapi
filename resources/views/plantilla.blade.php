f6a092436f1562f25066d3ad4b0f74de9026c319
silvanalope <silvialopez@unisangil.edu.co>
2021-08-08 09:55:28 -0500

    gix route

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -4,20 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <style>
     .active a {
       color:red;
       text-decoration: none;
     }
     </style>
-    
+
     <title>@yield('title', 'Aprendible')</title>
 </head>
 <body>
       <nav>
        <ul>
-         <li class = "{{request()->routels('home') ? 'active' : ''}}"><a href="/"> home</a></li>
+         <li class = "{{request()->route('home') ? 'active' : ''}}"><a href="/"> home</a></li>
          <li><a href="/about"> about</a></li>
          <li><a href="/portafolio"> portafolio</a></li>
          <li><a href="/contact"> contactos</a></li>
